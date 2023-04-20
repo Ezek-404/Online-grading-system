50dd7edd6dcbaa3bf0fec2e758f2f1e87b848cca
Ezek_RF <108383312+Ezek-404@users.noreply.github.com>
2023-04-20 12:59:21 +0800

    Create 404.php

---
+++ grading/404.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <!-- Custom fonts for this template-->
+        <link href="startbootstrap-sb-admin-2-gh-pages/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.gstatic.com/s/nunito/v24/XRXV3I6Li01BKofIOuaBXso.woff2"
+        rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="startbootstrap-sb-admin-2-gh-pages/css/sb-admin-2.min.css" rel="stylesheet">
+        <script src="startbootstrap-sb-admin-2-gh-pages/js/sb-admin-2.min.js"></script>
+        <script src="startbootstrap-sb-admin-2-gh-pages/vendor/jquery/jquery.min.js"></script>
+        <script src="startbootstrap-sb-admin-2-gh-pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>404 page not found</title>
+</head>
+<style type="text/css">
+	@import url('https://fonts.googleapis.com/css?family=Sniglet:400,800');
+
+		body{
+			background: #95A5A6;
+			font-family: 'Sniglet', cursive;
+
+		}
+		h1{
+			text-align: center;
+			font-size: 190px;
+			font-weight: 400;
+			margin: 0;
+		}
+		.fa{
+			font-size: 120px;
+			text-align: center;
+			display: block;
+			padding-top: 100px;
+			margin: 0 auto;
+			color: #A93226;
+		}
+		#error p{
+			text-align: center;
+			font-size: 36px;
+			color: #4A235A;
+		}
+		a.back{
+			text-align: center;
+			margin: 0 auto;
+			display: block;
+			text-decoration: none;
+			font-size: 24px;
+			background: #BA4A00;
+			border-radius: 10px;
+			width: 10%;
+			padding: 4px;
+			color: #fff; 
+
+		}
+		footer p{
+			padding-top: 160px;
+			text-align: center;
+		}
+		a.w3hubs{
+			text-decoration: none;
+			color: #A93226;
+		}
+		@media(max-width: 550px){
+			a.back{
+				width: 20%;
+			}
+		}
+		@media(max-width: 425px){
+			h1{
+				    padding-top: 20px;
+			}
+			.fa{
+				padding-top: 100px;
+			}
+		}
+	</style>
+<body>
+    <section id="error">
+        <div class="content">
+        <i class="fa fa-warning"></i>
+        <h1>404</h1>
+        <p>Error occurred! - File not Found</p>
+        <a class="back" href="javascript:history.back()">Back</a>
+        </div>
+    </section>
+</body>
+</html>
